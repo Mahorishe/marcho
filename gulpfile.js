434066cc4badd18b15eb74e8b52e8be96bfa7413
Mahorishe <mahorishe73@gmail.com>
2022-01-05 16:18:17 +0300

    task font converter and added header

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const uglify        = require('gulp-uglify');
 const imagemin      = require('gulp-imagemin');
 const del           = require('del');
 const sync          = require('browser-sync').create();
+const fs            = require('fs');
+const fonter        = require('gulp-fonter');
+const ttf2woff2     = require('gulp-ttf2woff2');
 
 const browserSync = () => {
   sync.init({
@@ -70,18 +73,86 @@ const reset = () => {
   return del('dist')
 }
 
+const otfToTtf = () => {
+  return src('src/fonts/*.otf')
+  .pipe(fonter({
+    formats: ['ttf']
+  }))
+  .pipe(dest('src/fonts/'))
+}
+
+const ttfToWoff = () => {
+  return src('src/fonts/*.ttf')
+  .pipe(fonter({
+    formats: ['woff']
+  }))
+  .pipe(dest('src/fonts/'))
+  .pipe(src('src/fonts/*.ttf'))
+  .pipe(ttf2woff2())
+  .pipe(dest('src/fonts/'))
+}
+
+const fontsStyle = () => {
+  let fontsFile = 'src/scss/_fonts.scss';
+  fs.readdir('src/fonts/', function (err, fontsFiles) {
+    if (fontsFiles) {
+      if (!fs.existsSync(fontsFile)){
+        fs.writeFile(fontsFile, '', cb);
+        let newFileOnly;
+        for (var i = 0; i < fontsFiles.length; i++) {
+          let fontFileName = fontsFiles[i].split('.')[0];
+          if (newFileOnly !== fontFileName) {
+            let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
+            let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
+            if (fontWeight.toLowerCase() === 'thin') {
+              fontWeight = 100;
+            } else if (fontWeight.toLowerCase() === 'extralight') {
+              fontWeight = 200;
+            } else if (fontWeight.toLowerCase() === 'light') {
+              fontWeight = 300;
+            } else if (fontWeight.toLowerCase() === 'medium') {
+              fontWeight = 500;
+            } else if (fontWeight.toLowerCase() === 'semibold') {
+              fontWeight = 600;
+            } else if (fontWeight.toLowerCase() === 'bold') {
+              fontWeight = 700;
+            } else if (fontWeight.toLowerCase() === 'extrabold' || fontWeight.toLowerCase() === 'heavy') {
+              fontWeight = 800;
+            } else if (fontWeight.toLowerCase() === 'black') {
+              fontWeight = 900;
+            } else {
+              fontWeight = 400;
+            }
+            fs.appendFile(fontsFile,`@font-face {\n\tfont-family: '${fontName}';\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
+            newFileOnly = fontFileName;
+          }
+        }
+      } else {
+        // Если файл есть, выводим сообщение
+        console.log("Файл scss/fonts.scss уже сушествуетю Для обновления необходимо его удалить");
+      }
+    }
+  });
+
+  return src('./src');
+  function cb() {}
+}
+
 const watcher = () => {
   watch(['src/scss/**/*.scss'], styles);
   watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts);
   watch(['src/**/*.html']).on('change', sync.reload);
 }
 
+const fonts = series(otfToTtf, ttfToWoff, fontsStyle);
+
 exports.styles = styles;
 exports.scripts = scripts;
-exports.sync = sync;
+exports.browserSync = browserSync;
 exports.images = images;
 exports.build = series(reset, images, build);
 exports.reset = reset;
+exports.fonts = fonts;
 exports.watcher = watcher;
 
-exports.default = parallel(styles, scripts, sync, watcher);
+exports.default = parallel(styles, scripts, browserSync, watcher);
