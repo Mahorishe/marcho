2ba2dff772fa7d559bdccb0a8fe543ee49c7336b
Mahorishe <mahorishe73@gmail.com>
2022-01-07 12:02:23 +0300

    added video-popup and product-items section

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,14 @@ $(function(){
     arrows: false,
     fade: true
   });
+
+  $(".star-rank").rateYo({
+    rating: 4.0,
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
 });
 
 
